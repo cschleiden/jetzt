7b7a802f231edef21d23f79bbe907f22b1bf30c7
Christopher Schleiden <cschleiden@live.de>
2021-02-21 19:53:12 -0800

    Use .default instead of .render

--- src/templates.ts
+++ src/templates.ts
@@ -1,11 +1,11 @@
-import { resolve } from "url";
 import { NextPage } from "./next";
+import { resolve } from "url";
 
 export function handler(pageName: string) {
   return `const page = require("./${pageName}");
 
 module.exports = async function (context) {
-    await page.render(context.bindings.req, context.res);
+    await page.default(context.bindings.req, context.res);
 };`;
 }
 
@@ -18,14 +18,14 @@ export function functionJson(page: NextPage) {
         direction: "in",
         name: "req",
         methods: ["get"],
-        route: page.processedRoute
+        route: page.processedRoute,
       },
       {
         type: "http",
         direction: "out",
-        name: "res"
-      }
-    ]
+        name: "res",
+      },
+    ],
   });
 }
 
@@ -34,9 +34,9 @@ export function hostJson(): string {
     version: "2.0",
     extensions: {
       http: {
-        routePrefix: ""
-      }
-    }
+        routePrefix: "",
+      },
+    },
   });
 }
 
@@ -44,13 +44,13 @@ export function proxiesJson(assetsUrl: string, pages: NextPage[]): string {
   const pageProxies: any = {};
 
   // Generate proxies for static pages
-  for (const p of pages.filter(p => p.isStatic && !p.isSpecial)) {
+  for (const p of pages.filter((p) => p.isStatic && !p.isSpecial)) {
     pageProxies[`proxy_${p.identifier}`] = {
       matchCondition: {
         methods: ["GET"],
-        route: p.route
+        route: p.route,
       },
-      backendUri: resolve(assetsUrl, `_next/pages/${p.targetPageFileName}`)
+      backendUri: resolve(assetsUrl, `_next/pages/${p.targetPageFileName}`),
     };
   }
 
@@ -60,18 +60,18 @@ export function proxiesJson(assetsUrl: string, pages: NextPage[]): string {
       page_assets: {
         matchCondition: {
           methods: ["GET"],
-          route: "_next/{*asset}"
+          route: "_next/{*asset}",
         },
-        backendUri: `${assetsUrl}_next/{asset}`
+        backendUri: `${assetsUrl}_next/{asset}`,
       },
       static_assets: {
         matchCondition: {
           methods: ["GET"],
-          route: "static/{*asset}"
+          route: "static/{*asset}",
         },
-        backendUri: `${assetsUrl}static/{asset}`
+        backendUri: `${assetsUrl}static/{asset}`,
       },
-      ...pageProxies
-    }
+      ...pageProxies,
+    },
   });
 }
